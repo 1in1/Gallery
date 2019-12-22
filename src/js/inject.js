3dabf7a4a41c41694f7fb4e5ba3996389b6af9ab
unknown <louie.gabriel@gmail.com>
2019-12-22 16:11:10 +0000

    Downloads working, also have alt-click functionality working#

--- src/js/inject.js
+++ src/js/inject.js
@@ -1,3 +1,6 @@
+//********************CONTENT SCRIPT FOR GALLERY EXTENSION********************
+//INCLUDES THE DIVS AND CSS WE NEED, THEN BUILDS THE NECESSARY DATA FOR
+//THE GALLERY OBJECT
 
 
 // Include the necessary div
@@ -26,44 +29,91 @@ for(i = 0; i < fileList.length; i++) {
   });
 }
 
+
 var options = {
     bgOpacity: 0.7,
     index: 0,
     escKey: true,
     arrowKeys: true,
     zoomEl: false,
-    shareEl: false,
-    shareButtons: {id:'download', label:'Download image', url:'{{raw_image_url}}', download:true}
+    shareEl: true,
+    //We are just hijacking the shareEl button to use as a download button,
+    //don't need the built-in functionality
+    shareButtons: {}
 };
 
 
 var pswpElement = document.querySelectorAll('.pswp')[0];
 var gallery;
 
+function launchGallery(index) {
+  console.log(index);
+  if (typeof(index) !== 'undefined')
+    options.index = index;
+
+  gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+  gallery.listen('close', function() {
+    options.index = gallery.getCurrentIndex();
+
+    new Noty({
+    type: 'info',
+    layout: 'topRight',
+    text: 'Jump to #' + parseInt(fileList[options.index].parentElement.id.substr(1)),
+    theme: 'relax',
+    timeout: 2500,
+    callbacks: { onClick: function($options) {
+        console.log(options.index);
+        fileList[options.index].scrollIntoView({
+          behaviour: 'smooth',
+          block: 'start'
+        });
+    } }
+    }).show();
+  });
+  gallery.init();
+}
+
+
+//Add event listeners
 document.addEventListener('keyup', (e) => {
   if (e.key === 'm' && e.getModifierState("Alt")) {
-    gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-    gallery.listen('close', function() {
-      options.index = gallery.getCurrentIndex();
-
-      new Noty({
-      type: 'info',
-      layout: 'topRight',
-      text: 'Jump to #' + parseInt(fileList[options.index].parentElement.id.substr(1)),
-      theme: 'relax',
-      timeout: 2500,
-      callbacks: { onClick: function($options) {
-          console.log(options.index);
-          fileList[options.index].scrollIntoView({
-            behaviour: 'smooth',
-            block: 'start'
-          });
-      } }
-      }).show();
-    });
-    gallery.init();
+    launchGallery();
   }
   else if (e.key === 'n' && e.getModifierState("Alt")) {
     gallery.close();
   }
+});
+
+document.addEventListener('click', (e) => {
+  if(e.target.tagName == 'IMG' && e.altKey) {
+    var targ = e.target.parentElement.parentElement.parentElement.parentElement.getElementsByClassName('file')[0];
+    for(i = 0; i < fileList.length; i++) {
+      if(targ == fileList[i])
+        launchGallery(i);
+    }
+  }
+});
+
+document.getElementsByClassName("pswp__button pswp__button--share")[0].addEventListener("click", function($gallery, $items) {
+  var i = gallery.getCurrentIndex();
+  //Probably safe to assume gallery is open if they've clicked the share button...
+  browser.runtime.sendMessage({index: i, url: items[i].src, title: items[i].title})
+  .then( () => {
+    new Noty({
+      type: 'success',
+      layout: 'centerRight',
+      text: 'Download started...',
+      theme: 'relax',
+      timeout: 2500
+      }).show();
+  }).catch( (p) => {
+    new Noty({
+      type: 'error',
+      layout: 'centerRight',
+      text: 'Failed to start download :(',
+      theme: 'relax',
+      timeout: 2500
+    }).show();
+    console.log(p);
+  });
 });
