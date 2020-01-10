4c9ceb1e30032a275f78c4f3da61f414e1554a62
Louie Gabriel <asg58@cam.ac.uk>
2020-01-10 17:15:43 +0000

    Build updated to package the ext for ff

--- build.rb
+++ build.rb
@@ -3,12 +3,12 @@
 require 'fileutils'
 require 'rest-client'
 
-FileUtils.rm_r Dir.glob('./dist/*')
-FileUtils.mkdir_p './dist/js/'
-FileUtils.cp './src/manifest.json', './dist/manifest.json'
-FileUtils.cp "./src/js/noty.min.js", "./dist/js/noty.min.js"
-FileUtils.cp "./src/js/photoswipe-ui-default.min.js", "./dist/js/photoswipe-ui-default.min.js"
-FileUtils.cp "./src/js/photoswipe.min.js", "./dist/js/photoswipe.min.js"
+FileUtils.rm_r Dir.glob('./build/*')
+FileUtils.mkdir_p './build/js/'
+FileUtils.cp './src/manifest.json', './build/manifest.json'
+FileUtils.cp "./src/js/noty.min.js", "./build/js/noty.min.js"
+FileUtils.cp "./src/js/photoswipe-ui-default.min.js", "./build/js/photoswipe-ui-default.min.js"
+FileUtils.cp "./src/js/photoswipe.min.js", "./build/js/photoswipe.min.js"
 
 css = Array.new
 psrootmini = RestClient.post "https://html-minifier.com/raw", {:input => File.open("./src/photoswipe-root.html").read}
@@ -17,7 +17,7 @@ css[1] = RestClient.post "https://cssminifier.com/raw", {:input => File.open("./
 css[2] = RestClient.post "https://cssminifier.com/raw", {:input => File.open("./src/style/noty.css").read}
 css[3] = RestClient.post "https://cssminifier.com/raw", {:input => File.open("./src/style/relax.css").read}
 
-mainout = File.open("./dist/js/inject.js", "w")
+mainout = File.open("./build/js/inject.js", "w")
 mainout.write("const markup = `")
 mainout.write(psrootmini.gsub(/\n/,""))
 mainout.write("`; const allCSS = `")
@@ -27,3 +27,15 @@ end
 mainout.write("`; ")
 injsrc = RestClient.post "https://javascript-minifier.com/raw", {:input => File.open("./src/js/inject.js").read}
 mainout.puts injsrc
+
+
+#Pack and sign for Firefox
+
+FileUtils.rm_r Dir.glob('./out/firefox/*')
+FileUtils.mkdir_p './out/firefox/'
+key = File.open("apikey.txt").readlines
+#system("web-ext build --source-dir .\\build --artifacts-dir .\\out\\firefox")
+system("web-ext sign --source-dir .\\build --artifacts-dir .\\out\\firefox --api-key #{key[0].chomp} --api-secret #{key[1].chomp}")
+
+FileUtils.mkdir_p './dist/firefox/'
+FileUtils.cp(Dir.glob('./out/firefox/*.xpi'), '.dist/firefox/')
